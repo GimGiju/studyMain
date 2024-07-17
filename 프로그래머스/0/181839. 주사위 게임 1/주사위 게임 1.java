b1d0c997b3946f33c4631974d25c75a9a9468c2f
GimGiju <155405743+GimGiju@users.noreply.github.com>
2024-07-17 20:37:34 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.12 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,13 +1,8 @@
 class Solution{
     public int solution(int a, int b) {
-        int answer = 0;
-        if (a % 2 != 0 && b % 2 != 0) {
-            answer = (int) (Math.pow(a, 2) + Math.pow(b, 2));
-        } else if (a % 2 != 0 || b % 2 != 0) {
-            answer = 2 * (a + b);
-        } else if (a % 2 == 0 && b % 2 == 0) {
-            answer = Math.abs(a - b);
-        }
+       int answer = (a % 2 != 0 && b % 2 != 0) ? (int) (Math.pow(a, 2) + Math.pow(b, 2)) :
+        (a % 2 != 0 || b % 2 != 0) ? 2 * (a + b) :
+                Math.abs(a - b);
         return answer;
     }
 }
