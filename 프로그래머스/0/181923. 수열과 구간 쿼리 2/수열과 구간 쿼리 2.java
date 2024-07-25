ebe0f1905f0f1d44860a89cfb1202ecb72d9536a
GimGiju <155405743+GimGiju@users.noreply.github.com>
2024-07-25 16:17:21 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 3.86 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,18 +1,13 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            int s = queries[i][0];
-            int e = queries[i][1];
-            int k = queries[i][2];
-            int min = Integer.MAX_VALUE;
-            for (int j = s; j <= e; j++) {
-                if (k < arr[j] && arr[j] < min) {     
-                    min = arr[j];
-                }
+       for (int[] query : queries) {       // 쿼리스 배열의 각 요소인 쿼리를 하나씩 순회
+            int s = query[0];               // 쿼리는 2개의 정수를 담고 있는 배열로 첫번째 정수는 시작 인덱스 s
+            int e = query[1];
+            int k = query[2];
+            for (int i = s; i <= e; i++) {      // 조건문으로 i를 s라고 선언하고 i는 e 보다 작거나 같으면서 i가 순회하면서
+                arr[i] += 1;        // arr를 1씩 증가 시킴  arr[i] = arr[i] + 1과 동일함
             }
-            answer[i] = min == Integer.MAX_VALUE ? -1 : min;       
         }
-        return answer;
+        return arr;
     }
 }
